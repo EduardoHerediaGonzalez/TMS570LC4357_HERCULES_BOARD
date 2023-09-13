82832138aabb8d3914d208b366759e1d9619fb63
Eduardo Heredia Gonzalez <EduardoHerediaGonzalez@gmail.com>
2023-09-13 14:07:59 -0600

    Source files of component GPIO modified.

--- source/BSP/LowDriver/GPIO/GPIO.c
+++ source/BSP/LowDriver/GPIO/GPIO.c
@@ -7,18 +7,21 @@
  *        Eduardo Heredia Gonzalez
  *        Diseñador           
  *
- * @brief   GPIO 
- * @version No oficial
+ * @brief   GPIO implementation file
+ * @version 1.0.0
  *     
  */
 /* system headers */
+#include "PLATFORM_TYPES_def.h"
+#include "STD_TYPES_def.h"
+#include "TMS570LC4357_def.h"
 
 /* used headers */
 
 /* own headers */
-#include "GPIO.h"
-#include "GPIO_cfg.h"
 #include "GPIO_def.h"
+#include "GPIO_cfg.h"
+#include "GPIO.h"
 
 /*[EXPORTED]****************************************************************************/
 
@@ -27,14 +30,61 @@
 /*[INTERNAL]****************************************************************************/
 
 /* internal__pre-processor macros */
+#define GPIO_s_nBASE_ADDRESS    ()
 
 /* internal__types */
+static uint8 GPIO_s_u8ModuleInitState = (uint8)STD_nOFF;
+static volatile uint32 *GPIO_s_pu32GIOGCR0 = (uint32*)GIO_nBASE_ADDRESS;
+
+typedef struct
+{
+    uint32 GIODIR;
+    uint32 GIODIN;
+    uint32 GIODOUT;
+    uint32 GIODSET;
+    uint32 GIODCLR;
+    uint32 GIOPDR;
+    uint32 GIOPULDIS;
+    uint32 GIOPSL;
+}volatile GPIO_s_tstCtrlRegs;
 
 /* private__variables */
 
 /* private__functions */
 
 /* public_functions */
+void GPIO_vInit(void)
+{
+    if(GPIO_s_u8ModuleInitState == (uint8)STD_nOFF)
+    {
 
+        GPIO_s_u8ModuleInitState = (uint8)STD_nON;
+    }
+    else
+    {
+        /* Nothing to do */
+    }
+}
+
+uint8 GPIO_u8SetState(uint8 u8Port, uint8 u8Pin, uint8 u8State)
+{
+    uint8 u8RetValue = (uint8)STD_nNOT_OK;
+
+    return u8RetValue;
+}
+
+uint8 GPIO_u8GetState(uint8 u8Port, uint8 u8Pin)
+{
+    uint8 u8RetValue = (uint8)STD_nUNDEFINED;
+
+    return u8RetValue;
+}
+
+GPIO_tstPortConfig* GPIO_u8GetPortConfig(uint8 u8Port)
+{
+    GPIO_tstPortConfig *pstRetValue = (GPIO_tstPortConfig*)NULL;
+
+    return pstRetValue;
+}
 
 /* EOF */
--- source/BSP/LowDriver/GPIO/GPIO_def.h
+++ source/BSP/LowDriver/GPIO/GPIO_def.h
@@ -7,8 +7,8 @@
  *        Eduardo Heredia Gonzalez
  *        Diseñador           
  *
- * @brief   GPIO_def 
- * @version No oficial
+ * @brief   GPIO_def definition file
+ * @version 1.0.0
  *     
  */
 
@@ -16,9 +16,47 @@
 #define GPIO_def_H_
 
 /* system headers */
+#include "PLATFORM_TYPES_def.h"
 
 /* exported macros */
 
 /* exported types */
+typedef enum
+{
+    GPIO_enPortA = 0,
+    GPIO_enPortB,
+    GPIO_enTotalPortName
+}GPIO_tenPortName;
+
+typedef enum
+{
+    GPIO_enPin0 = 0,
+    GPIO_enPin1,
+    GPIO_enPin2,
+    GPIO_enPin3,
+    GPIO_enPin4,
+    GPIO_enPin5,
+    GPIO_enPin6,
+    GPIO_enPin7,
+    GPIO_enTotalPinNumber
+}GPIO_tenPinNumber;
+
+typedef enum
+{
+    GPIO_enInputPullDown = 0,
+    GPIO_enInputPullUp,
+    GPIO_enInputPullDisable,
+    GPIO_enOutputPushPull,
+    GPIO_enOutputOpenDrain,
+    GPIO_enTotalPinMode
+}GPIO_tenPinMode;
+
+typedef struct
+{
+    uint8 u8PinBumber;
+    uint8 u8PinMode;
+    uint8 u8PinInitState;
+    uint8 u8Padding;
+}GPIO_tstPortConfig;
 
 #endif /* GPIO_def_H_ */
